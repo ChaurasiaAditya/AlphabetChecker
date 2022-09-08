be37e5954b55afb27fec1fcbf1e6c064278dcc69
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-08 22:57:23 +0530

    created print vowel or constant Method

--- src/main/java/com/aditya/AlphabetChecker.java
+++ src/main/java/com/aditya/AlphabetChecker.java
@@ -6,4 +6,18 @@
 package com.aditya;
 
 public class AlphabetChecker {
+
+    /**
+     * Prints "vowel" if the input character is a vowel, "consonant" if the input character is a consonant,
+     * and "invalid" if the input character is neither a vowel nor a consonant.
+     *
+     * @param alphabet
+     */
+    public void printVowelOrConsonant(char alphabet) {
+        if (alphabet == 'a' || (alphabet == 'e') || (alphabet == 'i') || (alphabet == 'o') || (alphabet == 'u')) {
+            System.out.println("vowel");
+        } else if (alphabet == 'b' || alphabet == 'c' || alphabet == 'd' || alphabet == 'f' || alphabet == 'g' || alphabet == 'h' || alphabet == 'j' || alphabet == 'k' || alphabet == 'l' || alphabet == 'm' || alphabet == 'n' || alphabet == 'p' || alphabet == 'q' || alphabet == 'r' || alphabet == 's' || alphabet == 't' || alphabet == 'v' || alphabet == 'w' || alphabet == 'x' || alphabet == 'y' || alphabet == 'z')
+            System.out.println("consonant");
+        else System.err.println("invalid");
+    }
 }
